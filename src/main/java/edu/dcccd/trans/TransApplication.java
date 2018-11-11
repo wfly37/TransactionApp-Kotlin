0e04183ea6b956d574774162fdb3d0e06dd10b57
bfly <billfly@programmer.net>
2018-11-10 18:35:30 -0600

    Initial commit.

---
+++ src/main/java/edu/dcccd/trans/TransApplication.java
@@ -0,0 +1,12 @@
+package edu.dcccd.trans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run(TransApplication.class, args);
+    }
+}
